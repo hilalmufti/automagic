1af57b41fbcb3b00cff43b31e942ae76eba40c5b
Hilal Mufti <hmufti@cs.washington.edu>
2024-07-09 23:51:55 -0700

    +symbolic differentiation

--- src/automagic.clj
+++ src/automagic.clj
@@ -1,5 +1,11 @@
 (ns automagic)
 
+(defn gcd [a b]
+  (if (= b 0)
+    a
+    (gcd b (mod a b))))
+
+
 (defn cons [x y]
   (defn dispatch [m]
     (cond (= m 0) x
@@ -14,7 +20,8 @@
   (z 1))
 
 (defn make-rat [n d]
-  (cons n d))
+  (let [g (gcd n d)]
+    (cons (/ n g) (/ d g))))
 
 (defn numer [x]
   (car x))
@@ -44,15 +51,65 @@
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
 
-(defn print-rat [x]
-  (println)
+(defn print-rat [x] 
   (print (numer x))
   (print "/")
-  (print (denom x)))
-
-(print-rat (div-rat (make-rat 3 2) (make-rat 1 4)))
+  (println (denom x)))
 
 (defn str-rat [x]
   (str (numer x) "/" (denom x)))
 
-(print-rat (sub-rat (make-rat 1 2) (make-rat 1 3)))
+(def one-half (make-rat 1 2))
+
+(print-rat one-half)
+
+(def one-third (make-rat 1 3))
+
+(print-rat (add-rat one-half one-third))
+
+(print-rat (mul-rat one-half one-third))
+
+(print-rat (add-rat one-third one-third))
+
+(def neg-one-third (make-rat 1 -3))
+
+(print-rat neg-one-third)
+
+(defn memq [item x] 
+  (cond (empty? x) false
+        (= item (first x)) x
+        :else (memq item (rest x))))
+
+(memq 'apple '(x (apple suace) y apple pear))
+
+(defn variable? [e])
+
+(defn same-variable? [v1 v2])
+
+(defn sum? [e])
+
+(defn addend [e])
+
+(defn augend [e])
+
+(defn make-sum [a1 a2])
+
+(defn product? [e])
+
+(defn multiplier [e])
+
+(defn multiplicand [e])
+
+(defn make-product [m1 m2])
+
+(defn deriv [exp var]
+  (cond (number? exp) 0
+        (variable? exp) (if (same-variable? exp var) 1 0)
+        (sum? exp) (make-sum (deriv (addend exp) var)
+                             (deriv (augend exp) var))
+        (product? exp) (make-sum
+                        (make-product (multiplier exp)
+                                      (deriv (multiplicand exp) var))
+                        (make-product (deriv (multiplier exp) var)
+                                      (multiplicand exp)))
+        :else (throw (Exception. "unknown expression type: DERIV"))))
